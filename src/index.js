e49851df8b0728497229dfe3abe73b362bbc2be4
asimqureshi-cpu <asim.qureshi@thebrainsyndicate.com>
2026-03-14 17:15:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,8 +16,7 @@ const server = http.createServer(app);
 // Open CORS for MVP
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 }));
 app.options('*', cors());
 
